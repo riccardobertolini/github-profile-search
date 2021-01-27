3cc20e7af2c8611bd6044dd5923015780567ef5b
Riccardo Bertolini <info@riccardobertolini.net>
2021-01-27 10:02:15 +0100

    Update App.test.tsx

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('app should render', () => {
   const wrapper = render(<App />);
   
   expect(wrapper).toMatchSnapshot();
